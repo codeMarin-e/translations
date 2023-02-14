c60c914034ff35d7ec8a8e842d1fa12c82b6cf7e
Marin <marin@dev.nddesign.no>
2023-02-14 16:25:00 +0200

    return cache for frontend again

--- src/stubs/project/app/Providers/marinar/translations_register.php
+++ src/stubs/project/app/Providers/marinar/translations_register.php
@@ -1,6 +1,6 @@
 <?php
 \Illuminate\Support\Facades\Gate::policy(\Waavi\Translation\Models\Translation::class, \App\Policies\TranslationPolicy::class);
 
-//if(request()->whereIAm() == 'admin') {
+if(request()->whereIAm() == 'admin') {
     config(['translator.cache.enabled' => false]);
-//}
+}
